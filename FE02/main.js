ef185391de90883c415b5f54dcbd97f00e08a45d
premrahi <premrahi234@gmail.com>
2025-12-20 20:03:15 +0530

    added FE02 Folder

---
+++ FE02/main.js
@@ -0,0 +1,21 @@
+ //var ,let , const
+
+const age = 20 ;
+console.log(age);
+
+const name = 'Prem' ;
+
+//concatenation
+console.log('my name is ' + name + ' and i am '+ age + ' years old')
+
+const hello = `my name is ${name} and i am ${age} years old` ;
+console.log(hello) ;
+
+// properties
+const s = 'hello world' ;
+console.log(s.length)
+
+// if we want a particular section
+console.log(s.substring(0,5).toUpperCase()) ;
+// (INCLUSIVE , EXCLUSIVE) ;
+
